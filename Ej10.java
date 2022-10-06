93b0b3a7ca24222071e8a98ee16d88575e6c29a8
KuzaFkto <kuzafkto@gmail.com>
2022-10-06 10:08:48 +0200

    error fixing

--- Ej10.java
+++ Ej10.java
@@ -6,7 +6,7 @@ public static void main(String[] args) {
 PrintWriter printWriter = new PrintWriter(System.out,true);
 printWriter.println("         ____");
 printWriter.println("        /___/\\_");
-printWriter.println("\033[93m "+ "\u266B "+"       _\\   \\/_/\\__");
+printWriter.println("\033[93m "+ "\254"+"       _\\   \\/_/\\__");
 printWriter.println("     __\\⬜⬜⬜⬜⬜⬜⬜\\/_/\\");
 printWriter.println("     \\⬜⬜⬜__⬜⬜⬜⬜__⬜\\⬜\\");
 printWriter.println("    __\\⬜⬜\\_\\⬜⬜⬜\\_\\⬜\\⬜\\⬜⬜⬜__");
